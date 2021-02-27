c4b92f4a39e007365c328d37ed9e80edb665d467
Jack Li <61096708+Jackiebibili@users.noreply.github.com>
2021-02-27 01:38:05 -0600

    fix bugs: blogs' files and leads are object arrays
    - narrow down to basic id arrays to pass to function
    - put site_db_name in requestContext for concise code
    - use the request object in query instead of req.body

--- controller/blogs.js
+++ controller/blogs.js
@@ -1,18 +1,34 @@
 const Storage = require('../service/storage')
 
 exports.getBlogEntry = async(req, res, next, dbClient) => {
-   const getBlog = await Storage.find(req.body, dbClient);
+   const getBlog = await Storage.find(req, dbClient);
    if(!getBlog.items) {
       //no items - return an empty array
       res.status(200).json(getBlog);
    }
    //restore the files info by id
-   const fileIds = getBlog.items.map((item) => item.files);
+   const fileIds = getBlog.items.map((item) => {
+      return item.files.reduce((acc, arr) =>{
+         if(arr.fileId) {
+            return [...acc, arr.fileId];
+         } else {
+            return acc;
+         }
+      }, []);
+   });
    const fullFiles = fileIds.map((filesId) => {
       return getListOfItems(filesId, 'file-label', req.body.requestContext.site_db_name, dbClient);
    });
    //restore the leads info by id
-   const leadIds = getBlog.items.map((item) => item.leads);
+   const leadIds = getBlog.items.map((item) => {
+      return item.leads.reduce((acc, arr) => {
+         if(arr.leadId) {
+            return [...acc, arr.leadId];
+         } else {
+            return acc;
+         }
+      }, []);
+   });
    const fullLeads = leadIds.map((leadsId) => {
       return getListOfItems(leadsId, 'officers', req.body.requestContext.site_db_name, dbClient);
    })
@@ -33,7 +49,12 @@ exports.updateBlogEntry = async (req, res, next, dbClient) => {
 
 const getListOfItems = async (ids, collectionName, site_db_name, dbClient) => {
    const items = ids.map((id) => {
-      return Storage.get({itemId: id, collectionName, site_db_name }, dbClient);
+      return Storage.get(
+         {
+            itemId: id,
+            collectionName,
+            requestContext: {site_db_name}
+         }, dbClient);
    });
    const results = await Promise.all(items);
    return results.map((item) => item.item);
