4a39e777b8dc956e61c2f87cd9a962480485acf4
Jack Li <61096708+Jackiebibili@users.noreply.github.com>
2021-05-30 10:16:28 -0500

    Add query/get one full blog by itemId
    - add get single full blog by itemId on the route /data/blogs/get
    - change the name of the handling function to emphasize the job it does
    - modularize lines of code into a helper function to share use across
    multiple functions

--- controller/blogs.js
+++ controller/blogs.js
@@ -1,47 +1,34 @@
 const Storage = require('../service/storage');
 
 exports.getBlogEntry = async (req, res, next, dbClient) => {
+  const getBlog = await Storage.get(req.body, dbClient);
+  const { fullFiles, fullLeads } = getBlogFullPromises(req, dbClient, [
+    getBlog.item,
+  ]);
+
+  const [files, leads] = await Promise.all([
+    Promise.all(fullFiles),
+    Promise.all(fullLeads),
+  ]);
+  getBlog.item = [getBlog.item].map((item, idx) => {
+    return { ...item, files: files[idx], leads: leads[idx] };
+  })[0];
+
+  // return json response
+  res.status(200).json(getBlog);
+};
+
+exports.findBlogEntry = async (req, res, next, dbClient) => {
   const getBlog = await Storage.find(req, dbClient);
   if (!getBlog.items) {
     // no items - return an empty array
     res.status(200).json(getBlog);
   }
-  // restore the files info by id
-  const fileIds = getBlog.items.map((item) => {
-    return item.files.reduce((acc, arr) => {
-      if (arr.fileId) {
-        return [...acc, arr.fileId];
-      } else {
-        return acc;
-      }
-    }, []);
-  });
-  const fullFiles = fileIds.map((filesId) => {
-    return getListOfItems(
-      filesId,
-      'file-label',
-      req.body.requestContext.site_db_name,
-      dbClient
-    );
-  });
-  // restore the leads info by id
-  const leadIds = getBlog.items.map((item) => {
-    return item.leads.reduce((acc, arr) => {
-      if (arr.leadId) {
-        return [...acc, arr.leadId];
-      } else {
-        return acc;
-      }
-    }, []);
-  });
-  const fullLeads = leadIds.map((leadsId) => {
-    return getListOfItems(
-      leadsId,
-      'officers',
-      req.body.requestContext.site_db_name,
-      dbClient
-    );
-  });
+  const { fullFiles, fullLeads } = getBlogFullPromises(
+    req,
+    dbClient,
+    getBlog.items
+  );
   // const files = await Promise.all(fullFiles);
   // const leads = await Promise.all(fullLeads);
   const [files, leads] = await Promise.all([
@@ -59,6 +46,48 @@ exports.getBlogEntry = async (req, res, next, dbClient) => {
 exports.updateBlogEntry = async (req, res, next, dbClient) => {
   const resUpdate = await Storage.update(req.body, dbClient);
   res.status(200).json(resUpdate.item);
+};
+
+const getBlogFullPromises = (req, dbClient, blogs) => {
+  // restore the files info by id
+  const fullFiles = blogs
+    .map((item) => {
+      return item.files.reduce((acc, arr) => {
+        if (arr.fileId) {
+          return [...acc, arr.fileId];
+        } else {
+          return acc;
+        }
+      }, []);
+    })
+    .map((filesId) => {
+      return getListOfItems(
+        filesId,
+        'file-label',
+        req.body.requestContext.site_db_name,
+        dbClient
+      );
+    });
+  // restore the leads info by id
+  const fullLeads = blogs
+    .map((item) => {
+      return item.leads.reduce((acc, arr) => {
+        if (arr.leadId) {
+          return [...acc, arr.leadId];
+        } else {
+          return acc;
+        }
+      }, []);
+    })
+    .map((leadsId) => {
+      return getListOfItems(
+        leadsId,
+        'officers',
+        req.body.requestContext.site_db_name,
+        dbClient
+      );
+    });
+  return { fullFiles, fullLeads };
 };
 
 const getListOfItems = async (ids, collectionName, site_db_name, dbClient) => {
